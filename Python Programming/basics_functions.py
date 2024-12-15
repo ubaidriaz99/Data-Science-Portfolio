9078586ad7f7f9fa6bcfc10b72faf4e0fcd79da3
ubaidriaz99 <130097752+ubaidriaz99@users.noreply.github.com>
2024-12-15 21:22:05 +0500

    Add files via upload

---
+++ Python Programming/basics_functions.py
@@ -0,0 +1,33 @@
+# x= input ("enter a number ")
+# x=int(x)
+# y=3
+# z=x+y
+# print (z, type (z))
+
+# if x==3:
+#  print ("ubaid")
+
+# def ubaiddd():
+#     x=3
+#     y=5
+#     z=x+y
+#     print(z)
+
+# ubaiddd ()
+
+# def riaz(age):
+#     if age<=25:
+#         print("ubaid")
+#     elif age>=30:
+#         print("uzair")
+#     else:
+#         print("family")
+# riaz(27)
+
+
+def future_age (age):
+    new=age+15
+    return new
+
+p=future_age(40)
+print(p)
---
+++ Python Programming/loops.py
@@ -0,0 +1,20 @@
+# x=1
+# while x<6:     #parenthesis not necessary
+#     print("ubaid", x)
+#     x=x+1
+
+
+# for x in range(5):
+#     print(x)
+
+# for x in range(5,10):
+  # print(x)
+
+# array 
+
+d=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+for i in d:
+   if(i=="Wed"):continue 
+   print(i)
+   
